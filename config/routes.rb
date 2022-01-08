690354617d7ee8917efada783af31145df0ebfaf
heyzec <61238538+heyzec@users.noreply.github.com>
2022-01-08 13:31:38 +0800

    Change routes and controllers for lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   scope '/api/v1' do
     resources :tasks
     resources :tags
-    resources :lists
+    resources :lists do
+      post 'create', to: 'lists#create_task'
+    end
     post '/signup', to: 'users#create'
     post '/signin', to: 'sessions#create'
     get '/signout', to: 'sessions#destroy'
